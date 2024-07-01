6f9399b0ec8ac8505ce4c3d31be9eb51190fad5e
sanathlokanath1991 <sanath.lokanath@collaberadigital.com>
2024-07-01 16:08:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,5 @@
-variable "awsRegion" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "vpcName" {
-  type        = string
-  description = "Name of the VPC to create"
-}
-
-variable "vpcCidrBlock" {
-  type        = string
-  description = "CIDR block for the VPC"
-}
-
-variable "subnetCidrBlock" {
-  type        = string
-  description = "CIDR block for the subnet"
-}
-
-variable "availabilityZone" {
-  type        = string
-  description = "AWS Availability Zone for the subnet"
-}
+variable "awsRegion" {}
+variable "vpcName" {}
+variable "vpcCidrBlock" {}
+variable "subnetCidrBlock" {}
+variable "availabilityZone" {}
